be402e288a21b94e09d97b66dc10553ebfe96d0a
javidsultanov <javidsultanov67@gmail.com>
2026-08-13 02:45:29 +0400

    Create Onboarding Struct

---
+++ Zoo App/Struct/Onboarding.swift
@@ -0,0 +1,14 @@
+//
+//  Onboarding.swift
+//  Zoo App
+//
+//  Created by Javid Sultanov on 13.08.26.
+//
+
+import Foundation
+
+struct Onboarding {
+    var onboardingImage: String
+    var onboardingTitle: String
+    var onboardingInfo: String
+}
