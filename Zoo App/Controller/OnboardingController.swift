f91d6395a3689fe5d22cbe2c48509efcb7a9d58f
javidsultanov <javidsultanov67@gmail.com>
2026-08-13 02:31:25 +0400

    Create Onboarding And TabBar Controllers

---
+++ Zoo App/Controller/OnboardingController.swift
@@ -0,0 +1,16 @@
+//
+//  OnboardingController.swift
+//  Zoo App
+//
+//  Created by Javid Sultanov on 13.08.26.
+//
+
+import UIKit
+
+class OnboardingController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ Zoo App/Controller/TabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarController.swift
+//  Zoo App
+//
+//  Created by Javid Sultanov on 13.08.26.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
