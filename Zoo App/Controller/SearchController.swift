48321d6112f4744aa5ae3dd0c29b0afea6256045
javidsultanov <javidsultanov67@gmail.com>
2026-08-14 19:22:39 +0400

    Create Home, Search And Favorites Controllers

---
+++ Zoo App/Controller/SearchController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchController.swift
+//  Zoo App
+//
+//  Created by Javid Sultanov on 14.08.26.
+//
+
+import UIKit
+
+class SearchController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
