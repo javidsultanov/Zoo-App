bd86ddd27c1c92f8a84bd6991718458fdded5661
javidsultanov <javidsultanov67@gmail.com>
2026-08-14 19:41:35 +0400

    Add TabBar rootViewController Logic

--- Zoo App/Controller/FavoritesController.swift
+++ Zoo App/Controller/FavoritesController.swift
@@ -12,18 +12,11 @@ class FavoritesController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configureUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureUI() {
+        view.backgroundColor = .secondarySystemBackground
+        navigationItem.title = "Favorites"
     }
-    */
-
 }
--- Zoo App/Controller/TabBarController.swift
+++ Zoo App/Controller/TabBarController.swift
@@ -12,5 +12,28 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureUI()
+    }
+    
+    private func configureUI() {
+        let homeController = HomeController()
+        homeController.tabBarItem = .init(title: "Home",
+                                          image: UIImage(systemName: "house"),
+                                          tag: 0)
+        let homeNavigation = UINavigationController(rootViewController: homeController)
+        
+        let searchController = SearchController()
+        searchController.tabBarItem = .init(title: "Search",
+                                            image: UIImage(systemName: "magnifyingglass"),
+                                            tag: 0)
+        let searchNavigation = UINavigationController(rootViewController: searchController)
+        
+        let favoritesController = FavoritesController()
+        favoritesController.tabBarItem = .init(title: "Favorites",
+                                               image: UIImage(systemName: "star"),
+                                               tag: 0)
+        let favoritesNavigation = UINavigationController(rootViewController: favoritesController)
+        
+        viewControllers = [homeNavigation, searchNavigation, favoritesNavigation]
     }
 }
